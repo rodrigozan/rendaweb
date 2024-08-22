000baea61c6565549eefc27709d089f94883867e
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-08-22 11:56:52 -0300

    Update server.ts: created server method

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,1 +1,5 @@
-console.log('First Start')
+import { App } from "./app"
+
+const PORT = process.env.PORT || 4000;
+
+export default new App().server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
