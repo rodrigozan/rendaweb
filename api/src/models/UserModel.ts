2c49380dd550382b5d5a9d2e4ce5fbb49eee12dc
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2024-08-22 12:10:14 -0300

    created UserModels

---
+++ api/src/models/UserModel.ts
@@ -0,0 +1,20 @@
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+import { IUser } from 'interfaces/global.interface';
+
+const userSchema = new Schema<IUser>({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    celular: { type: Number, required: true },
+    active: { type: Boolean, required: true, default: false},
+    role: { type: String, enum: ['user', 'admin', 'moderator', 'editor'], default: 'user' }
+});
+
+userSchema.methods.checkPassword = async function (password: string) {
+    return await bcrypt.compare(password, this.password);
+};
+
+
+export const UserModel = model('User', userSchema);
